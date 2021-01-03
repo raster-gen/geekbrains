4c711bb32c8f44cedc021838b130d644c8b3aea9
raster-gen <rasterroot@gmail.com>
2021-01-03 15:46:33 +0300

    Fixed the problem with duplicate numbers

--- lesson_4/task_4_2.py
+++ lesson_4/task_4_2.py
@@ -4,9 +4,9 @@
 
 
 def get_greater_than_prev(seq_list):
-    return [i for i in seq_list[1:] if i > seq_list[seq_list.index(i) - 1]]
+    return [i for ind, i in enumerate(seq_list[1:]) if i > seq_list[ind]]
 
 
 if __name__ == '__main__':
-    lst = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
+    lst = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 7, 78, 123, 55]
     print(get_greater_than_prev(lst))
