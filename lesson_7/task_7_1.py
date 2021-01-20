dd36da04ad43feecec51045d886e961f78dbf4e9
raster-gen <rasterroot@gmail.com>
2021-01-21 02:14:47 +0300

    task 1 completed

---
+++ lesson_7/task_7_1.py
@@ -0,0 +1,28 @@
+from itertools import zip_longest
+
+
+class Matrix:
+    def __init__(self, lst):
+        self.lst = lst
+
+    def __str__(self):
+        self.our_str = []
+        for i in self.lst:
+            for v in i:
+                self.our_str.append(str(v))
+                self.our_str.append('\t')
+            self.our_str.append('\n')
+        return ''.join(self.our_str)
+
+    def __add__(self, other):
+        self.sum_matrix = [(k+v for k, v in zip(x, y)) for x, y in zip(self.lst, other.lst)]
+        return Matrix(self.sum_matrix)
+
+
+our_lst = [[2, 3, 5], [4, 5, 6], [6, 7, 7], [3, 5, 6], [3], [4, 2], [4]]
+our_lst2 = [[5, 1, 7], [2, 4, 9], [0, 9, 1], [2, 1, 1], [2, 4, 5], [4]]
+m = Matrix(our_lst)
+m2 = Matrix(our_lst2)
+print(m)
+print(m2)
+print(m + m2)
